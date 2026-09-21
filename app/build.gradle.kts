0eeedf62de381ee72cb55f4243cd9edf5daee8f5
affushk <as2834892@gmail.com>
2026-09-21 12:24:44 +0530

    Bump launcher to V9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.altaf.ioslauncher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "8.0.0"
+        versionCode = 9
+        versionName = "9.0.0"
     }
 
     compileOptions {

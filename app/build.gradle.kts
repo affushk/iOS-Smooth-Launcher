351c4a00a74d74494d906d5a9af40b78920a1bc6
affushk <as2834892@gmail.com>
2026-09-24 22:57:23 +0530

    Release V18 notification gesture rebuild

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.altaf.designerhome"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "17.0.0"
+        versionCode = 18
+        versionName = "18.0.0"
     }
 
     compileOptions {

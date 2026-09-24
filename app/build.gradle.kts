ff27589b15bc35c177931641967d01c12ef7ac48
affushk <as2834892@gmail.com>
2026-09-24 23:20:14 +0530

    Release V20 direct system surfaces

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.altaf.designerhome"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "19.0.0"
+        versionCode = 20
+        versionName = "20.0.0"
     }
 
     compileOptions {

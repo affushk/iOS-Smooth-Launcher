f9b4ea6b662d895911f4d09acf2e262d243ff506
affushk <as2834892@gmail.com>
2026-09-25 08:34:28 +0530

    Release V23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.altaf.designerhome"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "22.0.0"
+        versionCode = 23
+        versionName = "23.0.0"
     }
 
     compileOptions {

098581a4e53e61c29eb584eb8a7a9e05567ff9a3
affushk <as2834892@gmail.com>
2026-09-25 16:11:36 +0530

    Release V29 four master themes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.altaf.designerhome"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "28.0.0"
+        versionCode = 29
+        versionName = "29.0.0"
     }
 
     compileOptions {

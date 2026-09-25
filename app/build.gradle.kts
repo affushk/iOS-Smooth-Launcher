3eb0923da67e9cfe85725a6ab9163e3b38e6a9ef
affushk <as2834892@gmail.com>
2026-09-25 17:30:59 +0530

    Start V30 structural themes release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.altaf.designerhome"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "29.0.0"
+        versionCode = 30
+        versionName = "30.0.0"
     }
 
     compileOptions {
